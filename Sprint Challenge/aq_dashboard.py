bc8126705428b3173b04042dc8e8c0157fab633d
Dennis <domoreburpees@icloud.com>
2023-07-19 16:47:29 -0600

    pycodestyle

--- Sprint Challenge/aq_dashboard.py
+++ Sprint Challenge/aq_dashboard.py
@@ -9,23 +9,26 @@ api = openaq.OpenAQ()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 DB = SQLAlchemy(app)
 
+
 class Record(DB.Model):
     id = DB.Column(DB.Integer, primary_key=True)
     datetime = DB.Column(DB.String(30), nullable=False)
     value = DB.Column(DB.Float, nullable=False)
 
     def __repr__(self):
-        return '< Time {} --- Value {} >'.format(self.datetime, self.value)
+        return '{}, {}'.format(self.datetime, self.value)
+
 
 with app.app_context():
     DB.create_all()  # Add this line
+
 
 @app.route('/refresh')
 def refresh():
     """Pull fresh data from Open AQ and replace existing data."""
     DB.drop_all()
     DB.create_all()
-    
+
     # Get data from OpenAQ, make Record objects with it, and add to db
     data = get_results('Los Angeles', 'pm25')
     for utc_datetime, value in data:
@@ -34,19 +37,26 @@ def refresh():
     DB.session.commit()
     return 'Data refreshed!'
 
-def get_results(city, parameter):
+
+def get_results(city='Los Angeles', parameter='pm25'):
     # Retrieve the data from the API
     status, body = api.measurements(city=city, parameter=parameter)
 
     # Create a list of (utc_datetime, value) tuples
-    results = [(item['date']['utc'], item['value']) for item in body['results']]
 
-    return results
+    # return results
+    if status == 200:
+        results = [(item['date']['utc'], item['value'])
+                   for item in body['results']]
+        return results
+    else:
+        return []
+
 
 @app.route('/')
 def root():
     """Base view."""
-     # Retrieve the list of tuples
+    # Retrieve the list of tuples
     # results = get_results('Los Angeles', 'pm25')
 
     """Main route, display potentially risky PM 2.5 values."""
@@ -54,10 +64,11 @@ def root():
     risky_records = Record.query.filter(Record.value >= 18).all()
 
     # Convert the records to (datetime, value) tuples
-    results = [(record.datetime, record.value) for record in risky_records]
+    # results = [(record.datetime, record.value) for record in risky_records]
 
     # Convert the list to a string and return it
-    return str(results)
+    return str(risky_records)
+
 
 if __name__ == '__main__':
     app.run()
